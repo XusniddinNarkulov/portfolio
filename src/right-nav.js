5d25778da47eaa465021ab72f5b518ef005b0275
XusniddinNarkulov <narkulof@gmail.com>
2022-04-26 09:04:52 +0500

    adjust icon

--- src/right-nav.js
+++ src/right-nav.js
@@ -5,13 +5,25 @@ import "./right-nav.css";
 class RightNav extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { shart: true };
   }
+
+  click = () => {
+    if (this.state.shart == true) {
+      document.documentElement.style.setProperty("--black", "#fff");
+      document.documentElement.style.setProperty("--white", "#000");
+      this.setState({ shart: false });
+    } else {
+      document.documentElement.style.setProperty("--black", "#000");
+      document.documentElement.style.setProperty("--white", "#fff");
+      this.setState({ shart: true });
+    }
+  };
 
   render() {
     return (
       <div className="right-nav">
-        <i className="adjust icon"></i>
+        <i onClick={this.click} className="adjust icon"></i>
         <div className="x-bar">
           <i className="bars icon"></i>
           <i className="x icon"></i>
